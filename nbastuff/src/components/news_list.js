94a4507030a383ee9920ebfb85cac0e8db28bd0b
Jane <iamjane.gmail.com>
2017-11-17 21:55:48 -0800

    add newslist component and json

--- nbastuff/src/components/news_list.js
+++ nbastuff/src/components/news_list.js
@@ -1,1 +1,14 @@
-news_list.js
+import React from 'react';
+
+const newsList = (props) => {
+
+  const items = props.news.map((item)=>{
+      return()
+  })
+
+  return(
+    <div>News list</div>
+  )
+}
+
+export default newsList;
