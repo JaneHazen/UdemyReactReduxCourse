bb3a39d74ddef94f9e0cc510a9e8226840902da1
Jane <iamjane.gmail.com>
2017-11-23 11:49:49 -0800

    redux working! beginning to map over cars after search

--- intermediate_redux/src/actions/index.js
+++ intermediate_redux/src/actions/index.js
@@ -1,8 +1,8 @@
 
-const url_root = 'http://localhost:3004'
+const URL_ROOT = 'http://localhost:3004'
 export function getCars(keywords){
 
-  const request = fetch(`${url_root}/carsIndex?q=${keywords}`, {method:'GET'})
+  const request = fetch(`${URL_ROOT}/carsIndex?q=${keywords}`, {method:'GET'})
   .then( response => response.json())
 
   return {
