2365429b366d55cd409736c204f44f98a6ff49e3
Jane <iamjane.gmail.com>
2017-11-22 22:12:06 -0800

    set up routes, fix router install issue

--- intermediate_redux/src/index.js
+++ intermediate_redux/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
 import App from './components/app';
+import Car from './components/car';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+  <div>
+    <Route exact path="/" component={App}></Route>
+    <Route path="/car/:id" component={Car}></Route>
+  </div>
+</BrowserRouter>
+, document.getElementById('root'));
