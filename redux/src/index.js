e3fc3b7fd7740a1102a30060d7cfa81e0d5d353b
Jane <iamjane.gmail.com>
2017-11-22 18:14:25 -0800

    add createStore, apply Middleware, and add Provider

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware()}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
